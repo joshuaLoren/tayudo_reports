a6c22a12e6e93e5764493a607bd2cf928279e2e9
Joshua Small <jsmall17@uncc.edu>
2018-11-13 03:55:15 +0000

    last Gemfile had bugs, here is an up to date Gemfile

--- Gemfile
+++ Gemfile
@@ -1,7 +1,6 @@
 source 'https://rubygems.org'
 
 
-#haml
 gem "haml"
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '4.2.10'
@@ -46,4 +45,3 @@ group :development do
   # Spring speeds up development by keeping your application running in the background. Read more: https://github.com/rails/spring
   gem 'spring'
 end
-
--- config/routes.rb
+++ config/routes.rb
@@ -1,23 +1,15 @@
 
 Rails.application.routes.draw do
  
-  get 'home/index'
-
-#   get 'welcome/index'
-  
-#   resources :articles
-    
-#   root 'welcome#index'
-#   
     resources :users
     resources :reports
     resources :precios
     resources :general
     
-    get 'home/index'
-    root 'home#index'
+    #get 'home/index'
+    #root 'home#index'
     
     #direct to reports homepage
-    #get 'reports/index'
-    #root 'reports#index'
+    get 'reports/index'
+    root 'reports#index'
 end
