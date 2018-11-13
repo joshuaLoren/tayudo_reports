e4220caa2a6f307abebbda9850836fc7142b66ba
Joshua Small <jsmall17@uncc.edu>
2018-11-13 04:21:43 +0000

    session controller added user can not login and logout

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,19 @@
 
 Rails.application.routes.draw do
- 
+    #'home/index'
+    root 'home#index'\
+    
     resources :users
+    resources :sessions, only: [:new, :create, :destroy]
     resources :reports
     resources :precios
     resources :general
     
-    #get 'home/index'
-    #root 'home#index'
+    get 'signup', to: 'users#new', as: 'signup'
+    get 'login', to: 'sessions#new', as: 'login'
+    get 'logout', to: 'sessions#destroy', as: 'logout'
     
     #direct to reports homepage
-    get 'reports/index'
-    root 'reports#index'
+    #get 'reports/index'
+    #root 'reports#index'
 end
